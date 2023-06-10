9688284ff9cbd50744843414934d322e53a4d633
ameena08 <ameenaami101@gmail.com>
2023-06-10 17:09:45 +0530

    Added automatic initialization of roles table

--- src/main/java/com/jwt/implementation/model/Role.java
+++ src/main/java/com/jwt/implementation/model/Role.java
@@ -9,13 +9,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name="role")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Role {
+public class Role implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
