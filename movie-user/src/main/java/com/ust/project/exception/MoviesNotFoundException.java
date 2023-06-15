48e4d4dcec9c0c768ac39f8bfb951c0c964397c7
245198 <127502947+245198@users.noreply.github.com>
2023-06-15 18:17:15 +0530

    Delete movie-user directory

--- movie-user/src/main/java/com/ust/project/exception/MoviesNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ust.project.exception;
-
-public class MoviesNotFoundException extends Exception {
-		public MoviesNotFoundException(String message) {
-	        super(message);
-	    }
-
-
-}
