5b15ae91780090c6366640f92ed0e9a4931a6f20
NikhilThomas7 <Nikhil.Thomas@ust.com>
2023-06-15 12:08:58 +0530

    updated the admin service with all entities

--- admin-service-aws/src/main/java/com/ust/project/repository/MovieRepository.java
+++ admin-service-aws/src/main/java/com/ust/project/repository/MovieRepository.java
@@ -1,15 +1,14 @@
 package com.ust.project.repository;
 
-
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
-import com.ust.project.model.Movie;
+import com.ust.project.dto.MovieDto;
+import com.ust.project.entity.Movie;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-    
-}
+public interface MovieRepository extends JpaRepository<Movie,Long>{
+	
 
+}
